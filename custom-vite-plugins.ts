8e0dba7f06e6464dd1d7cecda38341997c84f073
Joel Warrington <joelw@hey.com>
2025-06-15 00:57:25 -0600

    Update icons

--- custom-vite-plugins.ts
+++ custom-vite-plugins.ts
@@ -2,23 +2,6 @@ import fs from 'fs';
 import { resolve } from 'path';
 import type { PluginOption } from 'vite';
 
-// plugin to remove dev icons from prod build
-export function stripDevIcons (isDev: boolean) {
-  if (isDev) return null
-
-  return {
-    name: 'strip-dev-icons',
-    resolveId (source: string) {
-      return source === 'virtual-module' ? source : null
-    },
-    renderStart (outputOptions: any, inputOptions: any) {
-      const outDir = outputOptions.dir
-      fs.rm(resolve(outDir, 'dev-icon-32.png'), () => console.log(`Deleted dev-icon-32.png from prod build`))
-      fs.rm(resolve(outDir, 'dev-icon-128.png'), () => console.log(`Deleted dev-icon-128.png from prod build`))
-    }
-  }
-}
-
 // plugin to support i18n 
 export function crxI18n (options: { localize: boolean, src: string }): PluginOption {
   if (!options.localize) return null
--- vite.config.base.ts
+++ vite.config.base.ts
@@ -4,7 +4,7 @@ import { ManifestV3Export } from '@crxjs/vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, BuildOptions } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { stripDevIcons, crxI18n } from './custom-vite-plugins';
+import { crxI18n } from './custom-vite-plugins';
 import manifest from './manifest.json';
 import devManifest from './manifest.dev.json';
 import pkg from './package.json';
@@ -35,7 +35,6 @@ export default defineConfig({
     tailwindcss(),
     tsconfigPaths(),
     react(),
-    stripDevIcons(isDev),
     crxI18n({ localize, src: './src/locales' }),
   ],
   publicDir: resolve(__dirname, 'public'),
